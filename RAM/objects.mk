25619f361752923e0d4a1c419de2f870d56d45c5
finhaz <finhaz@foxmail.com>
2023-08-05 10:25:40 +0800

    .gitignore修改多余文件

--- RAM/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lrts2800_fpu32.lib -l"E:/Users/Sea/workspace_v11/pmsm_foc/rts2800_fpu32_fast_supplement.lib" -llibc.a
-
